ee671e2d025375a178c90fd919800fadceecd232
Alireza Pargol <a.r.pargol@gmail.com>
2024-08-10 14:50:14 +0330

    Create model and serializer for api and install swagger

--- task_management/settings.py
+++ task_management/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "tasks.apps.TasksConfig",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'title', 'description')
+    search_fields = ('title', 'description')
+    list_filter = ('completed',)
+    raw_id_fields = ('user',)
 
-# Register your models here.
